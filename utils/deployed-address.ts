bd2966dfc64c7f0125ae0004bbb9fb7dc3504c36
Aadityagigih45 GG <aadityagigih45@gmail.com>
2024-08-03 07:52:09 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xB383a2a1705454Cfe2dcB594eFE1a2eC5349A63f'
+export const ProxyAdmin = '0xF34E3e6069D8F4552a1BBd17957924cd70B4F3dE'
+export const SWTRImplementation = '0xB8BFd07ef55e6FbBE761cC7B236Ed7037856E096'
     
